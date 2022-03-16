492240058f5b1c8daeb1f0a876499300a72f55e4
sujeetgh <sujeetmehta6@gmail.com>
2022-03-16 12:05:31 +0530

    initial commit

---
+++ ekart.py
@@ -0,0 +1,24 @@
+import requests
+
+class Ekart():
+    """API for ekart
+
+    :param api_key: str
+    """
+
+    def __init__(self, api_key) -> None:
+        self._endpoint = "https://api.trackingmore.com"
+        self._headers = {
+                "Content-Type": "application/json",
+                "Trackingmore-Api-Key": api_key,
+                }
+
+    def get_status(self, order_id, carrier_code: str = "ekart") -> str:
+        """Gets static of order.
+
+        :param order_id
+        :carrier_code (optional)
+        """
+        return requests.get(f"{self._endpoint}/v2/trackings/{carrier_code}/{order_id}", headers=self._headers).text
+
+
---
+++ main.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import requests
+import os
+from ekart import Ekart
+import json
+import time
+
+
+load_dotenv()
+ORDER_ID = "FMPP1104066047"
+API_KEY = os.getenv("API_KEY")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+def send_msg(text, chat_id=-1001576544273):
+        requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}")
+        print("msg sent on telegram")
+
+
+def main():
+    ekart = Ekart(api_key=API_KEY)
+    last_event = None
+
+    while True:
+        status = ekart.get_status(ORDER_ID)
+        data = json.loads(status)
+        print(data)
+
+        update = data.get("data").get("lastEvent")
+        print(update)
+        if update != last_event:
+            last_event = update
+            send_msg(text=f"""<b>Update on chess delivery:<b>
+                    {last_event}""")
+
+        time.sleep(15*60)
+
+if __name__ == "__main__":
+    main()
+
